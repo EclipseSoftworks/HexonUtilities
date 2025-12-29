48d0baa6c2bbf8d43bfc68fd6e013b24df498214
EclipseSoftworks <eclipsesoftworksltd@gmail.com>
2025-12-29 06:04:59 +0000

    Update triggablix.js

--- api/triggablix.js
+++ api/triggablix.js
@@ -3,7 +3,7 @@ export default async function handler(req, res) {
     return res.status(405).json({ success: false, message: "Method Not Allowed" });
   }
 
-  const expectedApiKey = "sfdogsdkgopfdkgdspgksopgkdsgokdfs[gpsdkg[pofsdkgf[sdgsdfgisdjgfuopisjgusoighsfdupighfsdpiughdsfgudsfhgpiushgpdiughsdpighsdpigusdhgpiufsdhgupisdhgpdghupighsdfpighsdfpgjb5e8w9jb93w";
+  const expectedApiKey = "sfdogsdkgopfdkgdspgksopgkdsgokdfsgpsdkgpofsdkgfsdgsdfgisdjgfuopisjgusoighsfdupighfsdpiughdsfgudsfhgpiushgpdiughsdpighsdpigusdhgpiufsdhgupisdhgpdghupighsdfpighsdfpgjb5e8w9jb93w";
   const authHeader = req.headers.authorization || "";
   if (authHeader !== `Bearer ${expectedApiKey}`) {
     return res.status(401).json({ success: false, message: "Unauthorized" });
