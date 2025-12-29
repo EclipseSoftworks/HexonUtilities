43eece13ef96f84f86e6d1dfb63573a713622496
EclipseSoftworks <eclipsesoftworksltd@gmail.com>
2025-12-29 05:49:25 +0000

    Update triggablix.js

--- api/triggablix.js
+++ api/triggablix.js
@@ -31,7 +31,7 @@ export default async function handler(req, res) {
 
   const embed = {
     title: "Hexon Utilities",
-    color: 000000,
+    color: 8126685,
     fields: [
       {
         name: "Game",
